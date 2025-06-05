ae902ab9af88e3ceddef7783deae08617f4d39ad
dgrco <dantegrieco11@gmail.com>
2025-06-04 18:27:25 -0700

    changes to hypr-related configs; changes to nvim; changes to alacritty config

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -2,17 +2,4 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {
-  {
-    'danymat/neogen',
-    config = function()
-      require('neogen').setup {
-        enabled = true,
-      }
-      local opts = { noremap = true, silent = true }
-      vim.api.nvim_set_keymap('n', '<Leader>nf', ":lua require('neogen').generate()<CR>", opts)
-    end,
-    -- Uncomment next line if you want to follow only stable versions
-    -- version = "*"
-  },
-}
+return {}
